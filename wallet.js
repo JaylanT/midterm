c45c5c9ba80b1a28a1ec18849e97e3bbd1ab5413
JaylanT <Jaylan.Tse@gmail.com>
2016-10-08 00:22:51 -0700

    buy, sell implemented

--- wallet.js
+++ wallet.js
@@ -1,19 +1,92 @@
-var request = require('request');
-var _ = require('lodash');
+#!/usr/bin/env node
 
-var url = 'https://coinbase.com/api/v1/currencies/exchange_rates';
+const request = require('request');
+const _ = require('lodash');
+
+const URL = 'https://coinbase.com/api/v1/currencies/exchange_rates';
+const BUY = 'BUY';
+const SELL = 'SELL';
 
 function buy(amount, currency) {
-	request(url, (err, res, body) => {
-		if (!err && res.statusCode == 200) {
-			var conversions = JSON.parse(body);
+	initiateTransaction(amount, currency, BUY);
+}
 
-			if (currency) {
-				var key = currency + '_to_btc';
-				console.log(conversions[key]);
-			}
+function sell(amount, currency) {
+	initiateTransaction(amount, currency, SELL);
+}
+
+function orders() {
+
+}
+
+function initiateTransaction(amount, currency, type) {
+	if (isNaN(amount) || amount <= 0) {
+		console.log('No amount specified');
+		return;
+	}
+	
+	getBTCConversions((conversions) => {
+		const transaction = createTransaction(amount, currency, type, conversions);
+
+		if (transaction) {
+			ordersQueue.push(transaction);
+			printTransaction(transaction);
 		}
 	});
 }
 
+function createTransaction(amount, currency, type, conversions) {
+	const transaction = {
+		amount: amount,
+		timestamp: new Date().toUTCString(),
+		type: type
+	}
+
+	if (currency) {
+		const key1 = currency.toLowerCase() + '_to_btc';
+		const conversionValue = conversions[key1];
+		const key2 = 'btc_to_' + currency.toLowerCase();
+		const BTCValue = conversions[key2];
+
+		if (conversionValue === undefined) {
+			console.log('No known exchange rate for BTC/' + currency + '. Order failed');
+			return;
+		}
+
+		transaction.currency = currency.toUpperCase();
+		transaction.convertedValue = conversionValue;
+		transaction.BTCValue = BTCValue;
+	}
+
+	return transaction;
+}
+
+function getBTCConversions(callback) {
+	request(URL, (err, res, body) => {
+		if (!err && res.statusCode == 200) {
+			const conversions = JSON.parse(body);
+
+			callback(conversions);
+		}
+	});
+}
+
+function printTransaction(transaction) {
+	if (transaction.currency) {
+		console.log('Order to ' + transaction.type + ' ' + transaction.amount
+			+ ' ' + transaction.currency + ' worth of BTC queued @ '
+			+ transaction.BTCValue + ' BTC/' + transaction.currency
+			+ ' (' + transaction.convertedValue + ' BTC)');
+	} else {
+		console.log('Order to ' + transaction.type + ' ' + transaction.amount
+			+ ' BTC queued');
+	}
+}
+
+const ordersQueue = [];
+
 buy(20, 'usd');
+buy(46);
+buy('asdf');
+buy(20, 'asdf');
+buy(200, 'HKD');
